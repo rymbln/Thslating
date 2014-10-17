cb89743a3c1d30d8a30c8a3ca16d04654ef0d7b8
rymbln <rymbln@gmail.com>
2014-10-17 11:25:50 +0400

    Added Google translate

--- Thslating/Form1.cs
+++ Thslating/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace Thslating
 {
@@ -15,6 +17,40 @@ namespace Thslating
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Translate Text using Google Translate API's
+        /// Google URL - http://www.google.com/translate_t?hl=en&ie=UTF8&text={0}&langpair={1}
+        /// </summary>
+        /// <param name="input">Input string</param>
+        /// <param name="languagePair">2 letter Language Pair, delimited by "|".
+        /// E.g. "ar|en" language pair means to translate from Arabic to English</param>
+        /// <returns>Translated to String</returns>
+        public string TranslateText(
+            string input,
+            string languagePair)
+        {
+            string url = String.Format("http://www.google.com/translate_t?hl=en&ie=UTF8&text={0}&langpair={1}", input, languagePair);
+            WebClient webClient = new WebClient();
+            webClient.Encoding = System.Text.Encoding.UTF8;
+   
+            string result = webClient.DownloadString(url);
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(result);
+            string innerText1 = doc.DocumentNode.SelectSingleNode("//body").SelectNodes("//span").Single(n => n.Attributes.Any(a => a.Name == "id" && a.Value == "result_box")).InnerText;
+
+            result = innerText1;
+           
+           // result = result.Substring(result.IndexOf("id=result_box") + 22, result.IndexOf("id=result_box") + 500);
+          //  result = result.Substring(0, result.IndexOf("</div"));
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox2.Text = TranslateText(this.textBox1.Text, "ru|en");
         }
     }
 }
