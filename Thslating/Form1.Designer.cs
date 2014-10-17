74c59053609748cd8a9cf5ae364ac7145b09c252
rymbln <rymbln@gmail.com>
2014-10-17 15:00:25 +0400

    Added Excel Logic

--- Thslating/Form1.Designer.cs
+++ Thslating/Form1.Designer.cs
@@ -32,18 +32,28 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.tbxExcelFile = new System.Windows.Forms.TextBox();
+            this.btnSelectExcel = new System.Windows.Forms.Button();
+            this.lbxExcelColumns = new System.Windows.Forms.ListBox();
+            this.tbxSheetName = new System.Windows.Forms.TextBox();
+            this.lbxForTranslate = new System.Windows.Forms.ListBox();
+            this.btnFromTranslate = new System.Windows.Forms.Button();
+            this.btnToTranslate = new System.Windows.Forms.Button();
+            this.btnTranslateToExcel = new System.Windows.Forms.Button();
+            this.tbxTranslatedSheet = new System.Windows.Forms.TextBox();
+            this.tbxTranslatedExcel = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Location = new System.Drawing.Point(347, 15);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(259, 20);
             this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 40);
+            this.button1.Location = new System.Drawing.Point(347, 42);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(259, 23);
             this.button1.TabIndex = 1;
@@ -53,14 +63,14 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(12, 96);
+            this.textBox2.Location = new System.Drawing.Point(346, 98);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(259, 20);
             this.textBox2.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(13, 67);
+            this.button2.Location = new System.Drawing.Point(347, 69);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(258, 23);
             this.button2.TabIndex = 3;
@@ -68,11 +78,105 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // tbxExcelFile
+            // 
+            this.tbxExcelFile.Location = new System.Drawing.Point(13, 14);
+            this.tbxExcelFile.Name = "tbxExcelFile";
+            this.tbxExcelFile.Size = new System.Drawing.Size(208, 20);
+            this.tbxExcelFile.TabIndex = 4;
+            // 
+            // btnSelectExcel
+            // 
+            this.btnSelectExcel.Location = new System.Drawing.Point(227, 12);
+            this.btnSelectExcel.Name = "btnSelectExcel";
+            this.btnSelectExcel.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectExcel.TabIndex = 5;
+            this.btnSelectExcel.Text = "Excel";
+            this.btnSelectExcel.UseVisualStyleBackColor = true;
+            this.btnSelectExcel.Click += new System.EventHandler(this.btnSelectExcel_Click);
+            // 
+            // lbxExcelColumns
+            // 
+            this.lbxExcelColumns.FormattingEnabled = true;
+            this.lbxExcelColumns.Location = new System.Drawing.Point(13, 70);
+            this.lbxExcelColumns.Name = "lbxExcelColumns";
+            this.lbxExcelColumns.Size = new System.Drawing.Size(289, 316);
+            this.lbxExcelColumns.TabIndex = 6;
+            // 
+            // tbxSheetName
+            // 
+            this.tbxSheetName.Location = new System.Drawing.Point(13, 44);
+            this.tbxSheetName.Name = "tbxSheetName";
+            this.tbxSheetName.Size = new System.Drawing.Size(289, 20);
+            this.tbxSheetName.TabIndex = 7;
+            // 
+            // lbxForTranslate
+            // 
+            this.lbxForTranslate.FormattingEnabled = true;
+            this.lbxForTranslate.Location = new System.Drawing.Point(346, 136);
+            this.lbxForTranslate.Name = "lbxForTranslate";
+            this.lbxForTranslate.Size = new System.Drawing.Size(259, 251);
+            this.lbxForTranslate.TabIndex = 8;
+            // 
+            // btnFromTranslate
+            // 
+            this.btnFromTranslate.Location = new System.Drawing.Point(308, 280);
+            this.btnFromTranslate.Name = "btnFromTranslate";
+            this.btnFromTranslate.Size = new System.Drawing.Size(33, 107);
+            this.btnFromTranslate.TabIndex = 9;
+            this.btnFromTranslate.Text = "<---";
+            this.btnFromTranslate.UseVisualStyleBackColor = true;
+            this.btnFromTranslate.Click += new System.EventHandler(this.btnFromTranslate_Click);
+            // 
+            // btnToTranslate
+            // 
+            this.btnToTranslate.Location = new System.Drawing.Point(308, 167);
+            this.btnToTranslate.Name = "btnToTranslate";
+            this.btnToTranslate.Size = new System.Drawing.Size(33, 107);
+            this.btnToTranslate.TabIndex = 10;
+            this.btnToTranslate.Text = "--->";
+            this.btnToTranslate.UseVisualStyleBackColor = true;
+            this.btnToTranslate.Click += new System.EventHandler(this.btnToTranslate_Click);
+            // 
+            // btnTranslateToExcel
+            // 
+            this.btnTranslateToExcel.Location = new System.Drawing.Point(637, 95);
+            this.btnTranslateToExcel.Name = "btnTranslateToExcel";
+            this.btnTranslateToExcel.Size = new System.Drawing.Size(229, 23);
+            this.btnTranslateToExcel.TabIndex = 11;
+            this.btnTranslateToExcel.Text = "btnTranslateToExcel";
+            this.btnTranslateToExcel.UseVisualStyleBackColor = true;
+            this.btnTranslateToExcel.Click += new System.EventHandler(this.btnTranslateToExcel_Click);
+            // 
+            // tbxTranslatedSheet
+            // 
+            this.tbxTranslatedSheet.Location = new System.Drawing.Point(637, 45);
+            this.tbxTranslatedSheet.Name = "tbxTranslatedSheet";
+            this.tbxTranslatedSheet.Size = new System.Drawing.Size(229, 20);
+            this.tbxTranslatedSheet.TabIndex = 12;
+            // 
+            // tbxTranslatedExcel
+            // 
+            this.tbxTranslatedExcel.Location = new System.Drawing.Point(637, 69);
+            this.tbxTranslatedExcel.Name = "tbxTranslatedExcel";
+            this.tbxTranslatedExcel.Size = new System.Drawing.Size(229, 20);
+            this.tbxTranslatedExcel.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(894, 517);
+            this.Controls.Add(this.tbxTranslatedExcel);
+            this.Controls.Add(this.tbxTranslatedSheet);
+            this.Controls.Add(this.btnTranslateToExcel);
+            this.Controls.Add(this.btnToTranslate);
+            this.Controls.Add(this.btnFromTranslate);
+            this.Controls.Add(this.lbxForTranslate);
+            this.Controls.Add(this.tbxSheetName);
+            this.Controls.Add(this.lbxExcelColumns);
+            this.Controls.Add(this.btnSelectExcel);
+            this.Controls.Add(this.tbxExcelFile);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
@@ -90,6 +194,16 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbxExcelFile;
+        private System.Windows.Forms.Button btnSelectExcel;
+        private System.Windows.Forms.ListBox lbxExcelColumns;
+        private System.Windows.Forms.TextBox tbxSheetName;
+        private System.Windows.Forms.ListBox lbxForTranslate;
+        private System.Windows.Forms.Button btnFromTranslate;
+        private System.Windows.Forms.Button btnToTranslate;
+        private System.Windows.Forms.Button btnTranslateToExcel;
+        private System.Windows.Forms.TextBox tbxTranslatedSheet;
+        private System.Windows.Forms.TextBox tbxTranslatedExcel;
     }
 }
 
--- Thslating/Form1.cs
+++ Thslating/Form1.cs
@@ -10,12 +10,30 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
 
 
 namespace Thslating
 {
     public partial class Form1 : Form
+
+
     {
+        private List<string> lstColumns = new List<string>();
+        private List<string> lstColumnsForTranslate = new List<string>();
+        private List<CellElement> sheetData;
+        private string sheetName = "";
+        private class RowElement
+        {
+            public List<CellElement> CellElement;
+        }
+        private class CellElement
+        {
+            public string Name;
+            public string Value;
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -36,7 +54,7 @@ namespace Thslating
             string url = String.Format("http://www.google.com/translate_t?hl=en&ie=UTF8&text={0}&langpair={1}", input, languagePair);
             WebClient webClient = new WebClient();
             webClient.Encoding = System.Text.Encoding.UTF8;
-   
+
             string result = webClient.DownloadString(url);
 
             HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
@@ -44,9 +62,9 @@ namespace Thslating
             string innerText1 = doc.DocumentNode.SelectSingleNode("//body").SelectNodes("//span").Single(n => n.Attributes.Any(a => a.Name == "id" && a.Value == "result_box")).InnerText;
 
             result = innerText1;
-           
-           // result = result.Substring(result.IndexOf("id=result_box") + 22, result.IndexOf("id=result_box") + 500);
-          //  result = result.Substring(0, result.IndexOf("</div"));
+
+            // result = result.Substring(result.IndexOf("id=result_box") + 22, result.IndexOf("id=result_box") + 500);
+            //  result = result.Substring(0, result.IndexOf("</div"));
             return result;
         }
         public string TranslateYandexText(
@@ -54,7 +72,7 @@ namespace Thslating
          string language)
         {
             string userKey = "trnsl.1.1.20141017T075601Z.8466c9109acea7a9.43d3aaa48f8730c642fb563b8676aefcb3536aba";
-            string url = String.Format("https://translate.yandex.net/api/v1.5/tr/translate?key={0}&text={1}&lang={2}", userKey, input,language);
+            string url = String.Format("https://translate.yandex.net/api/v1.5/tr/translate?key={0}&text={1}&lang={2}", userKey, input, language);
 
             WebClient webClient = new WebClient();
             webClient.Encoding = System.Text.Encoding.UTF8;
@@ -77,6 +95,170 @@ namespace Thslating
         private void button2_Click(object sender, EventArgs e)
         {
             this.textBox2.Text = TranslateYandexText(this.textBox1.Text, "ru-en");
+        }
+
+        private void btnSelectExcel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlgOpenFile = new OpenFileDialog();
+
+            dlgOpenFile.InitialDirectory = Application.ExecutablePath.ToString();
+            dlgOpenFile.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            dlgOpenFile.FilterIndex = 1;
+            dlgOpenFile.RestoreDirectory = true;
+
+            if (dlgOpenFile.ShowDialog() == DialogResult.OK)
+            {
+                Excel.Application ExcelApplication = new Excel.Application();
+                this.tbxExcelFile.Text = dlgOpenFile.FileName.ToString();
+                Excel.Workbook theWorkbook = ExcelApplication.Workbooks.Open(dlgOpenFile.FileName, 0, true, 5, "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, false, false);
+                Excel.Sheets sheets = theWorkbook.Worksheets;
+                Excel.Worksheet worksheet = (Excel.Worksheet)sheets.get_Item(1);
+                sheetName = worksheet.Name;
+                
+                tbxSheetName.Text = sheetName;
+                tbxTranslatedSheet.Text = sheetName;
+                tbxTranslatedExcel.Text = dlgOpenFile.FileName.ToString();
+
+                lstColumns = GetColumnsFromExcel(worksheet);
+            
+                
+                //sheetData = new List<CellElement>();
+                //sheetData = GetDataFromExcelSheet(worksheet, lstColumns);
+
+                this.lbxExcelColumns.Items.Clear();
+                foreach (var item in lstColumns)
+                {
+                    this.lbxExcelColumns.Items.Add(item);
+                }
+                this.lbxExcelColumns.Refresh();
+                this.lbxForTranslate.Items.Clear();
+                this.lbxForTranslate.Refresh();
+
+                while (Marshal.ReleaseComObject(theWorkbook) > 0)
+                { }
+                while (Marshal.ReleaseComObject(theWorkbook) > 0)
+                { }
+
+
+                ExcelApplication.Quit();
+
+                while (Marshal.ReleaseComObject(ExcelApplication) > 0)
+                { }
+
+                MessageBox.Show("Data readed");
+            }
+        }
+
+        private List<CellElement> GetDataFromExcelSheet(Excel.Worksheet sheet, List<string> columnsList)
+        {
+            var lstRows = new List<CellElement>();
+
+            int rowCount = sheet.UsedRange.Rows.Count;
+            int columnCount = sheet.UsedRange.Columns.Count;
+            var cellElem = new CellElement();
+            for (int i = 2; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= columnCount; j++)
+                {
+                    lstRows.Add(new CellElement { Name = columnsList[j-1], Value = ((sheet.Cells[i, j].Value == null) ? "" : sheet.Cells[i, j].Value == null).ToString() });
+                }
+            }
+
+
+            return lstRows;
+
+        }
+        private List<string> GetColumnsFromExcel(Excel.Worksheet sheet)
+        {
+            int rowCount = sheet.UsedRange.Rows.Count;
+            int columnCount = sheet.UsedRange.Columns.Count;
+
+            var columnValue = new List<string>();
+
+            for (int i = 1; i <= columnCount; i++)
+            {
+                columnValue.Add(sheet.Cells[1,i].Value.ToString());
+            }
+
+            return columnValue;
+        }
+
+        private void btnToTranslate_Click(object sender, EventArgs e)
+        {
+            lbxForTranslate.Items.Add(lbxExcelColumns.SelectedItem);
+            lstColumnsForTranslate.Add(lbxExcelColumns.SelectedValue.ToString());
+        }
+
+        private void btnFromTranslate_Click(object sender, EventArgs e)
+        {
+            lbxForTranslate.Items.Remove(lbxForTranslate.SelectedItem);
+            lstColumnsForTranslate.Remove(lbxExcelColumns.SelectedValue.ToString());
+        }
+
+        private void btnTranslateToExcel_Click(object sender, EventArgs e)
+        {
+
+     
+            var filename = tbxTranslatedExcel.Text;
+
+            Excel.Application objExcel = new Excel.Application();
+            objExcel.ScreenUpdating = false;
+            objExcel.DisplayAlerts = false;
+            objExcel.Workbooks.Add();
+            Excel.Workbook objWorkbook = objExcel.Workbooks[1];
+            Excel.Worksheet objSheet = (Excel.Worksheet)objWorkbook.Sheets.get_Item(1);
+            objSheet.Name = tbxTranslatedSheet.Text;
+
+            for (int i = 1; i <= lstColumns.Count; i++)
+            {
+                objSheet.Cells[1, i].Value = lstColumns[i];
+            }
+
+            Excel.Application ExcelApplication = new Excel.Application();
+              Excel.Workbook theWorkbook = ExcelApplication.Workbooks.Open(tbxExcelFile.Text, 0, true, 5, "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, false, false);
+            Excel.Sheets sheets = theWorkbook.Worksheets;
+            Excel.Worksheet sheet = (Excel.Worksheet)sheets.get_Item(1);
+
+
+            int rowCount = sheet.UsedRange.Rows.Count;
+            int columnCount = sheet.UsedRange.Columns.Count;
+            var cellElem = new CellElement();
+            for (int i = 2; i <= rowCount; i++)
+            {
+                for (int j = 1; j <= columnCount; j++)
+                {
+                    cellElem = new CellElement { Name = lstColumns[j - 1], Value = ((sheet.Cells[i, j].Value == null) ? "" : sheet.Cells[i, j].Value == null).ToString() };
+                    if (lstColumnsForTranslate.Contains(cellElem.Name))
+                    {
+                        cellElem.Value = TranslateGoogleText(cellElem.Value, "ru|en");
+                    }
+                    objSheet.Cells[i, j].Value = cellElem.Value;
+                }
+            }
+                objWorkbook.SaveAs();
+                objWorkbook.SaveAs(tbxExcelFile.Text, Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, false, false, Excel.XlSaveAsAccessMode.xlNoChange, Excel.XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+
+                objWorkbook.Close();
+                theWorkbook.Close();
+                while (Marshal.ReleaseComObject(objWorkbook) > 0)
+                { }
+                while (Marshal.ReleaseComObject(objWorkbook) > 0)
+                { }
+                while (Marshal.ReleaseComObject(theWorkbook) > 0)
+                { }
+                while (Marshal.ReleaseComObject(theWorkbook) > 0)
+                { }
+
+                    objExcel.Quit();
+
+                while (Marshal.ReleaseComObject(objExcel) > 0)
+                { }
+
+                ExcelApplication.Quit();
+
+                while (Marshal.ReleaseComObject(ExcelApplication) > 0)
+                { }
+                MessageBox.Show("Перевод окончен");
         }
     }
 }
